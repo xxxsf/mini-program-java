69cef09a7b7691b582904dd863284ec78edc21a5
allan91coder <allan91coder@gmail.com>
2026-05-29 08:07:16 +0800

    feat(invoice): share invoice detail with read-only mode for recipients
    - Add share mode detection in invoice detail page
    - Share recipients see plain text view without buttons
    - Add share button for normal users to share invoices
    - Add share styles (green share button)
    - Enable share in JSON config

--- invoiceTool_miniProgram/pages/invoice/detail/index.js
+++ invoiceTool_miniProgram/pages/invoice/detail/index.js
@@ -4,10 +4,16 @@ var util = require('../../../utils/util.js')
 Page({
   data: {
     invoice: null,
-    statusText: ''
+    statusText: '',
+    showActions: true, // 是否显示操作按钮
+    plainText: '' // 纯文本格式
   },
 
   onLoad: function (options) {
+    // 检测是否从分享场景打开
+    var scene = options.scene || '';
+    var fromShare = options.from === 'share' || scene === '1044' || scene === '1007' || scene === '1008';
+    
     if (options.id) {
       var invoices = app.globalData.invoices
       for (var i = 0; i < invoices.length; i++) {
@@ -22,14 +28,34 @@ Page({
             'void': '已作废'
           }
 
+          // 生成纯文本
+          var plainText = this.formatInvoicePlainText(inv);
+
           this.setData({
             invoice: inv,
-            statusText: statusMap[inv.status] || '正常'
+            statusText: statusMap[inv.status] || '正常',
+            showActions: !fromShare,
+            plainText: plainText
           })
           break
         }
       }
     }
+  },
+
+  // 生成发票纯文本
+  formatInvoicePlainText: function (inv) {
+    var text = '发票详情\n';
+    text += '========================\n';
+    text += '金额：¥' + (inv.amount || '0.00') + '\n';
+    text += '发票号码：' + (inv.invoiceNo || '') + '\n';
+    text += '开票日期：' + (inv.dateStr || '') + '\n';
+    text += '销售方：' + (inv.sellerName || '') + '\n';
+    text += '购买方：' + (inv.buyerName || '') + '\n';
+    if (inv.category) {
+      text += '发票类型：' + inv.category + '\n';
+    }
+    return text;
   },
 
   onDelete: function () {
@@ -48,6 +74,25 @@ Page({
         }
       }
     })
+  },
+
+  // 分享给好友
+  onShareAppMessage: function () {
+    var invoice = this.data.invoice;
+    var title = invoice ? ('发票：¥' + invoice.amount + ' - ' + invoice.sellerName) : '发票详情';
+    return {
+      title: title,
+      path: '/pages/invoice/detail/index?id=' + (invoice ? invoice.id : '')
+    };
+  },
+
+  // 分享到朋友圈
+  onShareTimeline: function () {
+    var invoice = this.data.invoice;
+    return {
+      title: invoice ? ('发票：¥' + invoice.amount) : '发票详情',
+      query: 'id=' + (invoice ? invoice.id : '')
+    };
   },
 
   // 查看原件
